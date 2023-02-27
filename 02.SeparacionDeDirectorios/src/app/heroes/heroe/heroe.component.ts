4967ccea45ed738d824bf57fd5d98c567d83820a
Euclides Perez <eperez@intecol.com.co>
2023-02-27 17:12:01 -0500

    Se hace vinding unidireccional

---
+++ 02.SeparacionDeDirectorios/src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent{
+    nombre:string = 'Ironman';
+    edad:number = 45;
+
+    obtenerNombre():string{
+        return `${this.nombre} - ${this.edad}`
+      
+    }
+    get nombreCapitalizado(){
+        return this.nombre.toUpperCase()
+    }
+
+    cambiarNombre():void{
+        this.nombre = 'Spiderman'
+    }
+    cambiarEdad(){
+        this.edad = 25;
+    }
+}
---
+++ 02.SeparacionDeDirectorios/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  // pra comprender la directiva *ngFor crearemos un arreglo de heroes
+
+  heroes: string[] = ['Ironman', 'Spiderman', 'Goku'];
+
+  heroeBorrado:string = '';
+
+  borrarHeroe(){
+   this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
