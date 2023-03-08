28fe91640b0596f59daecdaa6fd2f2038a39f038
euclidesseg <euder0206@gmail.com>
2023-03-07 23:14:23 -0500

    se crean todos los modulos y components necesarios apra la logica

---
+++ 04.GifsApp/src/app/gifs/gifs.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { SearchGifsComponent } from './search-gifs/search-gifs.component';
+import { ListGifsComponent } from './list-gifs/list-gifs.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPageComponent,
+    SearchGifsComponent,
+    ListGifsComponent
+  ],
+  exports: [
+    GifsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
---
+++ 04.GifsApp/src/app/gifs/search-gifs/search-gifs.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-gifs',
+  templateUrl: './search-gifs.component.html',
+})
+export class SearchGifsComponent {
+
+}
