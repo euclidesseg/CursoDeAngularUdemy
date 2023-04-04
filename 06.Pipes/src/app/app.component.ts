2acca1231d4cf60f03d10d5d5ae075c4d62bd70f
Euclides Perez <eperez@intecol.com.co>
2023-04-04 17:31:19 -0500

    se agrega la libreria primeng y primeicon se aprende como se usa

--- 06.Pipes/src/app/app.component.ts
+++ 06.Pipes/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
+  nombre : string = 'Euclides seGundo Perez FernandeZ'
+  valor  : number = 1000
+  objeto = {
+    nombre : 'euclides'
+  }
+  getNombre(){
+    console.log(this.nombre)
+  }
 }
