15e0a5e0876a65a442844406d497f945d9eecc9a
Euclides Perez <eperez@intecol.com.co>
2023-05-05 10:47:45 -0500

    Se agrega peticion para obtener por usuario.

--- 07.heroesApp/src/app/herores/pages/hero-page/hero-page.component.ts
+++ 07.heroesApp/src/app/herores/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styleUrls: ['./hero-page.component.css']
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit{
 
+  public hero?:Hero
+
+  constructor(
+    private heroService:HeroesService,
+    private activateRoute:ActivatedRoute,
+    // el servicio de activateRoute nos permite obtener la ruta que nos llega a esta 
+    // componente atravez de la ruta de este mismo.
+    private router:Router,
+    ){}
+
+  ngOnInit(): void {
+    this.getHeroe()
+  }
+
+
+  getHeroe(){
+    //obtenemos acceso a todos los parametros de la ruta como un observable
+    this.activateRoute.params
+    .pipe(
+      switchMap((params) => this.heroService.getHeroById(params['id']))
+
+      // tambien puedo desestructurar el obgeto de parametros de la ruta
+      // switchMap(({id}) => this.heroService.getHeroById(id))
+    )
+    .subscribe((hero) =>{
+      if(!hero) return this.router.navigate(['/heroes/list']);
+      console.log(hero)
+      this.hero = hero;
+      return;
+    })
+
+  }
 }
--- 07.heroesApp/src/app/herores/services/heroes.service.ts
+++ 07.heroesApp/src/app/herores/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environment.prod';
 
@@ -13,5 +13,19 @@ export class HeroesService {
 
   getHeero():Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+  getHeroById(id:string):Observable<Hero|undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    // devido a que esta peticion puede retornar un err ya que el id puede no existir agregamos 
+    // el operador pipe para atrapar el error
+    .pipe(
+      catchError((error) => of(undefined))
+      );
+      // of() es una función que se utiliza para crear un nuevo Observable
+      // que emite uno o varios valores de forma síncrona
+      // emite los valores proporcionados como argumentos en orden
+      // De esta manera, si ocurre un error en el flujo de datos, 
+      // el Observable devolverá el valor undefined en lugar del error. 
+      // Si da error voy a regresar un observable que retorna undefined
   }
 }
