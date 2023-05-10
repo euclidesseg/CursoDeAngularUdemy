8ced60ffd3b03530e2f242918ca22ef427cda832
Euclides Perez <eperez@intecol.com.co>
2023-05-10 09:11:12 -0500

    Se termina la seccion 14 con la creacion del formulario para agregar o actualizar heroe

--- 07.heroesApp/src/app/herores/pages/new-page/new-page.component.ts
+++ 07.heroesApp/src/app/herores/pages/new-page/new-page.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public publishers = [
+    {id: 'DC Comics', value: 'Dc - Comics'},
+    {id: 'Marvel Comics', value: 'Marvel - Comics'}
+  ]
 }
