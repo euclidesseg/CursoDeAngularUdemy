a03f8f2f7fbd9ebdf1015a6f52ba864dc08df857
Euclides Perez <eperez@intecol.com.co>
2023-05-25 11:07:13 -0500

    git commit se hace proceso para login

---
+++ 07.heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, tap} from 'rxjs';
+
+import { environments } from '../../../environments/environment';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser():User|undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
+  }
+
+  login( email: string, password: string ):Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
+      );
+  }
+
+
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+}
