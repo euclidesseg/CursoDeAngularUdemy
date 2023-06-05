c1653abeb9a2c72db4ddb8adab539440e692f9cb
Euclides Perez <eperez@intecol.com.co>
2023-06-05 09:00:08 -0500

    Se agrega routingModule a la aplicacion

--- 08.formsReactives/src/app/app-routing.module.ts
+++ 08.formsReactives/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'reactive', loadChildren:() => import('./reative/reative.module').then(module => module.ReativeModule)},
+  {path:'auth', loadChildren:() => import('./auth/auth.module').then(module => module.AuthModule)},
+  {path:'**', redirectTo:'reactive'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
