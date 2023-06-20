f4368e606cdf49cb721d209220d28e3c925cb6a2
Euclides Perez <eperez@intecol.com.co>
2023-06-20 08:52:27 -0500

    Fin de seeccion con formularios dinamicos

--- 08.formsReactives/src/app/reative/pages/dinamic-page/dinamic-page.component.ts
+++ 08.formsReactives/src/app/reative/pages/dinamic-page/dinamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dinamic-page.component.html',
@@ -7,6 +7,8 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DinamicPageComponent {
   constructor(private formBuilder: FormBuilder) { }
+
+
 
   public myForm: FormGroup = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -17,15 +19,35 @@ export class DinamicPageComponent {
     // el arreglo seria ['Metal Gear', 'Death Stranding'] ya que los segundos corchetes indican solo las validaciones
     // y el valor por defecto que tendra cada dato
   })
+  public newFavorite : FormControl = new FormControl('',[Validators.required])
+
   // si usaramos solo formGroup seria de la siguiente manera
   // public myForm2 = new FormGroup({
   //   favoriteGames: new FormArray([])
   // })
 
   // getter para obtener los datos del favoiteGame
+  
+  
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray; // indico que el control que va a retornar es un form array ya que es un form array sus elementos tambien seran controls
   }
+
+  // Eliminar un elemento de un formArray
+  onDeleteFavorite(index: number):void{
+    this.favoriteGames.removeAt(index); // llamamos a nustro getter que ya lo tenemos
+  }
+
+  // agregar elementos a un formarray
+  onAddToFavorite():void{
+    if(this.newFavorite.invalid) return;
+    // extraemos el valor del formcontrol
+    const newGame:any = this.newFavorite.value;
+    this.favoriteGames.push(this.formBuilder.control(newGame,Validators.required));
+
+    this.newFavorite.reset();
+  }
+
   onSubmit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
@@ -35,15 +57,25 @@ export class DinamicPageComponent {
       console.log(this.myForm.value);
       this.myForm.reset();
     }
+   ( this.myForm.controls['favoriteGames'] as FormArray) = this.formBuilder.array([])
+   this.myForm.reset()
   }
+
+
   isValidField(nombreDeCampo: string): boolean | null {
     return this.myForm.controls[nombreDeCampo].errors && this.myForm.controls[nombreDeCampo].touched
   }
+
+
+  
   // Este metodo es el mismo de formularios basicos pero devemos tener en cuenta que este validara un controlArray 
   // entonces agregaremos un nuevo metodo
   isValidFieldInArray(formArray: FormArray, index: number): boolean | null { // nesetio saber cual es el arreglo de controles y saber cual es el indice que esta fallando
     return formArray.controls[index].errors && formArray.controls[index].touched
   }
+
+
+
   getFieldError(nombreDeCampo: string): string | null {
     if (!this.myForm.controls[nombreDeCampo]) return null
     else {
@@ -60,4 +92,7 @@ export class DinamicPageComponent {
       return ''
     }
   }
+
+
+
 }
