85199b6fc070c48d1b71af12f6498c6bc41b2106
euclidesseg <euder0206@gmail.com>
2023-06-22 08:57:50 -0500

    inicio seccion validaciones

--- 08.formsReactives/src/app/reative/pages/switches-page/switches-page.component.ts
+++ 08.formsReactives/src/app/reative/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  constructor(){}
+
+  public myForm : FormGroup = new FormGroup({
+    gender: new FormControl('M', [Validators.required]),
+    wantNotifications: new FormControl('M', [Validators.required]),
+    termsAndNotifications: new FormControl('M', [Validators.requiredTrue]) // que tiene que ser un valor verdadero
+  });
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }  
+    else{
+      console.log(this.myForm.value)
+    }
+  }
 }
