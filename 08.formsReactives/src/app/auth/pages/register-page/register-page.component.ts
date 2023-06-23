c762f2a38ef34baea2445e540a361671431407c7
intecolEu <eperez@intecol.com.co>
2023-06-23 18:31:04 -0500

    inicio de formulario para usuarios y de validaciones personalizadas

--- 08.formsReactives/src/app/auth/pages/register-page/register-page.component.ts
+++ 08.formsReactives/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+    constructor(private fBuilder : FormBuilder){}
+
+
+
+    public myForm : FormGroup = this.fBuilder.group({
+      name : ['',[Validators.required] ], // para name usaremos una expresion regular
+      email : ['',[Validators.required] ], // para email usaremos una expresion regular
+      username : ['',[Validators.required] ],
+      password : ['',[Validators.required, Validators.maxLength(8)] ], // para password usaremos una expresion regular
+      password2 : ['',[Validators.required, Validators.minLength(8)] ],
+    })
+
+    isValidField(){
+      // TODO: obtener desde un servicio
+    }
+
+    onSubmit(){
+      this.myForm.markAllAsTouched();
+    }
 }
